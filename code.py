fec41047de0c4db72468829e00b5ca033ea2b950
Boyong Feng (封博雍) <boyongfeng2@rhs.school.nz>
2026-06-07 23:59:59 +1200

    Deliverable 7
    - Added Brightness adjustment function
    
    - Added Light toggle 
    
    - Optimized some code
    
    - Fixed Encoder not snapping back to previous selection when backing out from submenu.
    
    - Improved Debugging LED feature
    
    - Main Menu Reworked
    
    - Submenu reworked

--- code.py
+++ code.py
@@ -14,7 +14,7 @@ from adafruit_display_text import label
 from adafruit_displayio_sh1106 import SH1106
 
 # Initialize the pixel strip
-pixels = neopixel.NeoPixel(board.GP18, 8, brightness=0, auto_write=False)
+pixels = neopixel.NeoPixel(board.GP18, 8, brightness=0.2, auto_write=False)
 
 red = (255, 0, 0)
 orange = (255, 64, 0)
@@ -36,6 +36,8 @@ gay_list7 = [magenta, red, orange, yellow, green, cyan, blue, purple]
 
 cycle = 0
 
+pixels.fill((0,0,0))
+pixels.show()
 
 led = digitalio.DigitalInOut(board.GP10)
 led.direction = digitalio.Direction.OUTPUT
@@ -108,7 +110,6 @@ bitmap, palette = adafruit_imageload.load(
     bitmap=displayio.Bitmap,
     palette=displayio.Palette
 )
-
 tile_main_s1 = displayio.TileGrid(
     bitmap, 
     pixel_shader=palette, 
@@ -117,24 +118,23 @@ tile_main_s1 = displayio.TileGrid(
 )
 #imageload---------------------------------------------
 
-#text label--------------------------------------------
-weeks = 0
-
-#text label--------------------------------------------
 
 #display constant--------------------------------------
 main_group = displayio.Group()
 display.root_group = main_group
-#display constant--------------------------------------
-
 main_menu = [tile_main_s1, tile_main_s2, tile_main_s3]
-
 main_group.append(main_menu[0])
 
+#lovibabeles---------------------------------------------
 layer = 0
+debug = True
+dir = 0
+briper = int(pixels.brightness * 100)
+pixelState = 0
+main_menu_state = 0
+colourState = (255, 255, 255)
 
-
-def gay_beam(cycle, selectron, layer):
+def gay_beam(cycle, main_menu_state, layer):
     # rainbow animation
     pixels[0] = gay_list0[cycle]
     pixels[1] = gay_list1[cycle]
@@ -155,11 +155,11 @@ def gay_beam(cycle, selectron, layer):
     pixels.show()
 
     # brightness / LED logic
-    if selectron == 2 and layer == 1:
+    if main_menu_state == 1 and layer == 1:
         led.value = True
         pixels.brightness = 0
 
-    elif selectron == 3:
+    elif main_menu_state == 2:
         if layer == 1:
             pixels.brightness = 1
             led.value = False
@@ -174,76 +174,141 @@ def gay_beam(cycle, selectron, layer):
     return cycle
 
 def gilded_beam():
-    pixels.fill((255, 130, 0))
+    pixels.fill(yellow)
     pixels.brightness = 1
     pixels.show()
     time.sleep(0.01)
     pixels.fill((0, 0, 0))
     pixels.brightness = 0
     pixels.show()
+
+def debug_print():
+    led.value = True
+    time.sleep(0.005)
+    led.value = False
+
+def pixel_switch(what):
+    global pixelState
+    if what == "on":
+        pixels.fill(colourState)
+        pixels.show()
+        pixelState = 1
+    elif what == "off":
+        pixels.fill((0,0,0))
+        pixels.show()
+        pixelState = 0
+
         
-#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-while True:
-    current_click = button1.value
-    current_back = button_back.value
-    current_C = buttonC.value
-
-    if layer == 0:
-        current_position = encoder.position
-
-
-    selectron = (current_position % 3) + 1
-    
-
-    text_area = label.Label(
+text_area_brightness = label.Label( #filler
         terminalio.FONT,
-        text= f"{selectron} weeks remaining",
+        text= f"BRIGHTNESS [{briper}%]",
         scale = 1,
         x=20,
         y=display.height//2
     )
-#control variables-_---------------------------------------------
+text_area_mode = label.Label( #filler
+        terminalio.FONT,
+        text= f"MODE SHOW",
+        scale = 1,
+        x=40,
+        y=display.height//2
+    )
+text_area_LED = label.Label( #filler
+        terminalio.FONT,
+        text= f"LED SHOW",
+        scale = 1,
+        x=40,
+        y=display.height//2
+    )        
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+while True:
+    text_area_brightness.text = f"BRIGHTNESS [{briper}%]"
 
-#rotary switch---------------------------------------------
+    current_click = button1.value
+    current_back = button_back.value
+    current_C = buttonC.value
 
-    if current_position != last_position:
+    current_position = encoder.position
+
+    if not current_C and last_C_state: #give confirm button a purpose
+            if pixelState == 0:
+                pixel_switch("on")
+            elif pixelState == 1:
+                pixel_switch("off")
+            if debug == True:
+                    debug_print()
+
+    if layer == 0:        
+        
+        #rotary switch---------------------------------------------
+        if current_position != last_position:
+            main_menu_state = current_position % 3
             main_group.pop()
-            main_group.append(main_menu[current_position % 3])
+            main_group.append(main_menu[main_menu_state])
             last_position = current_position
-            time.sleep(0.05)
-#rotary switch---------------------------------------------
-    
-    
-    
-#push button----------------------------------------------
+        #rotary switch---------------------------------------------
 
-    if not current_click and last_click_state: #press
-        main_group.pop()
-        main_group.append(text_area)
-        led.value = True
-        time.sleep(0.005)
-        led.value = False
-        layer = 1
+        if not current_click and last_click_state:
+            if main_menu_state == 0: 
+                main_group.pop()
+                main_group.append(text_area_brightness)
+                dir = 1
+                last_position = current_position
+            if main_menu_state == 1: 
+                main_group.pop()
+                main_group.append(text_area_mode)
+                dir = 2
+            if main_menu_state == 2: 
+                main_group.pop()
+                main_group.append(text_area_LED)
+                dir = 3
+            if debug == True:
+                debug_print()
+            layer = 1
 
-    if not current_back and last_back_state and layer == 1: #back
-        main_group.pop()
-        main_group.append(main_menu[current_position % 3])
-        last_position = -1 #resets the position so that the display updates when the rotary switch is turned again after pressing the back button
-        led.value = True
-        time.sleep(0.005)
-        led.value = False
-        layer = 0
-    
-    if not current_C and last_C_state: #gilded beam
-        led.value = True
-        time.sleep(0.005)
-        led.value = False
-        gilded_beam()
-    
+    if layer == 1:
+        if dir == 1:
+            if current_position != last_position:
+
+                change = -(current_position - last_position)
+                
+                briper += 2*change
+
+                if briper < 0:
+                    briper = 0
+                elif briper > 100:
+                    briper = 100
+
+                pixels.brightness = briper / 100
+                pixels.show()
+
+                text_area_brightness.text = f"BRIGHTNESS [{briper}%]"
+                last_position = current_position
+
+        
+                   
+        #if dir == 2:
+            #last_position = -1
+        #if dir == 3:
+            #last_position = -1
+
+        if not current_back and last_back_state:
+            main_group.pop()
+            main_group.append(main_menu[main_menu_state])
+            layer = 0
+            dir = 0
+            encoder.position = main_menu_state
+            last_position = encoder.position
+
+            if debug == True:
+                debug_print()
+            
+
     last_click_state = current_click
     last_back_state = current_back
     last_C_state = current_C
 
-    cycle = gay_beam(cycle, selectron, layer)
-    
+    #Experimental functions
+    #cycle = gay_beam(cycle, main_menu_state, layer)
+    #gilded_beam()
 #push button----------------------------------------------
